96e6f324d9b4c25a30a4b1a9ad56867539fabf3b
LucioArgiro <aleargiro3@gmail.com>
2025-12-29 19:16:29 -0300

    Fix:arreglos en el footer

--- src/components/footerComponent.jsx
+++ src/components/footerComponent.jsx
@@ -34,7 +34,7 @@ function footerComponent() {
         <h3>Contacto</h3>
          <Form>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
+        <Form.Label>Direccion de email</Form.Label>
         <Form.Control type="email" placeholder="name@example.com" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
