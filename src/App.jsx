ea70e095dc39ec67a50c64dad26c8411212e6460
Nazarena <nazarenamolina2@gmail.com>
2026-01-26 16:51:04 -0300

    edicion 1 de landing page terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HeaderComponent from './components/headerComponent.jsx';
 import FooterComponent from './components/footerComponent.jsx';
+import HomePage from './pages/HomePage.jsx';
 import './App.css';
 import './index.css';
 
@@ -12,7 +13,9 @@ function App() {
         <div className="app-container">
           <HeaderComponent />
           <main className="main-content">
-
+            <Routes>
+              <Route path='/' element={<HomePage/>}/>
+            </Routes>
           </main>
         </div>
         <FooterComponent />
