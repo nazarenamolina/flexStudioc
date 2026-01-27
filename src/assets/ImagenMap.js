03854c7825e00673dba8d05f5f954a31241e8e7d
Nazarena <nazarenamolina2@gmail.com>
2026-01-26 22:32:58 -0300

    logo de index.html

--- src/assets/ImagenMap.js
+++ src/assets/ImagenMap.js
@@ -2,7 +2,6 @@ import Logo from '../assets/img/Logo.png';
 import fondo from '../assets/img/fondo.png';
 import Banner from '../assets/img/Banner.png';
 import QuienSoy from '../assets/img/QuienSoy.png';
-import prueba from '../assets/img/prueba.PNG';
 import gimnasta from '../assets/img/gimnasta.png';
 import patinadora from '../assets/img/patinadora.jpeg';
 import acrobata from '../assets/img/acrobata.jpeg';
@@ -11,6 +10,7 @@ import deportista from '../assets/img/deportista.jpeg';
 import progresiva from '../assets/img/progresiva.jpeg';
 import titulo from '../assets/img/titulo.png';
 import logofooter from '../assets/img/logofooter.png';
+import loguito from '../assets/img/loguito.png';
 
 
 const ImageMap ={
@@ -18,7 +18,6 @@ const ImageMap ={
     fondo,
     Banner,
     QuienSoy,
-    prueba,
     gimnasta,
     patinadora,
     acrobata,
@@ -26,7 +25,8 @@ const ImageMap ={
     deportista, 
     progresiva,
     titulo, 
-    logofooter
+    logofooter,
+    loguito
 }
 
 export default ImageMap;
